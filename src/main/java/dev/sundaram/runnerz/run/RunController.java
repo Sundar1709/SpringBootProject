2c208b0709dbb85b3fc9a7dc9cc233183e084f09
sundaram <sundaramk961@gmail.com>
2024-04-01 15:48:09 +0530

    Added Validation functionality

--- src/main/java/dev/sundaram/runnerz/run/RunController.java
+++ src/main/java/dev/sundaram/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package dev.sundaram.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -27,19 +28,19 @@ public class RunController {
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Run not found");
+            throw new RunNotFoundException();
         }
        return run.get();
     }
     //post
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run){
+    void create(@Valid @RequestBody Run run){
         runRepository.create(run);
     }
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id){
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id){
         runRepository.update(run,id);
     }
     //delete
