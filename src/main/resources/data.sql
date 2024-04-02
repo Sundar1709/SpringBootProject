20155a72fe2a7bd568b443d5f12c530e34a0a066
sundaram <sundaramk961@gmail.com>
2024-04-02 07:12:23 +0530

    Added sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-Insert into Run(id,title,started_on,compleyted_on,miles,location)
+Insert into Run(id,title,started_on,completed_on,miles,location)
 values(1,'morning run','2024-01-01 06:00:00','2024-01-01 08:00:00',5,'outdoor');
